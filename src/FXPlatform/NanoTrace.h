562e5965d483b9b47d67d35e777af38b1a67aed0
Eric Zinda <ericz@inductorsoftware.com>
2019-05-18 14:43:56 -0700

    Updates to make InductorProlog easier to merge with InductorParser

--- src/FXPlatform/NanoTrace.h
+++ src/FXPlatform/NanoTrace.h
@@ -5,6 +5,7 @@
 #include <mutex>
 #include "ReflectionEnum.h"
 #include <sstream>
+#include "SystemTraceType.h"
 #include "Utilities.h"
 #include <vector>
 using namespace std;
@@ -13,19 +14,6 @@ using namespace std;
 Designed as a lightweight tracing mechanism that allows for outputting different levels of detail (TraceDetail)
 And different kinds of traces (SystemTraceType) for debugging purposes
 */
-
-// Types < Custom are reserved for the system
-// This is a bitfield
-#define SYSTEM_TRACE_TYPE(item) \
-    item(SystemTraceType, None, 0) \
-    item(SystemTraceType, System, 1) \
-    item(SystemTraceType, Parsing, 32) \
-    item(SystemTraceType, Custom, 0x00000800) \
-    item(SystemTraceType, HTML, (uint64_t)SystemTraceType::Custom * 8) \
-    item(SystemTraceType, Solver, (uint64_t)SystemTraceType::Custom * 4096) \
-    item(SystemTraceType, Unifier, (uint64_t)SystemTraceType::Custom * 8192) \
-    item(SystemTraceType, All, 0x0FFFFFFF)
-DECLARE_ENUM(SystemTraceType,SYSTEM_TRACE_TYPE)
 
 enum class TraceDetail
 {
